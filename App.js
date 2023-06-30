321f21e803cafcb24b3d72f830de3be8ce7c25ee
Emmanuel <38895255+Nixhazel@users.noreply.github.com>
2023-06-30 09:05:46 +0100

    updated react-native

--- App.js
+++ App.js
@@ -4,75 +4,9 @@ import Goalpage from "./src/components/Goalpage";
 // import styles from "./src/styles";
 
 export default function App() {
-  // const [enteredGoalText, setEnteredGoalText] = useState("");
-  // const [courseGoals, setCourseGoals] = useState([]);
-
-	// function goalInputHandler(enteredText) {
-	// 	setEnteredGoalText(enteredText);
-	// }
-
-	// function addGoalHandler() {
-	// 	setCourseGoals((currentCourseGoals) => [
-	// 		...currentCourseGoals,
-	// 		enteredGoalText,
-  //   ]);
-	// }
 	return (
-		// <View style={styles.appContainer}>
-		// 	<View style={styles.inputContainer}>
-		// 		<TextInput
-		// 			placeholder='Your Course Goals...'
-		// 			style={styles.textInput}
-		// 			onChangeText={goalInputHandler}
-		// 		/>
-		// 		<Button title='Add Goal' onPress={addGoalHandler} />
-		// 	</View>
-		// 	<View style={styles.goalsContainer}>
-		// 		{courseGoals.map((goal) => (
-		// 			<View key={goal} style={styles.goalItem}>
-		// 				<Text style={styles.goalText}>{goal}</Text>
-		// 			</View>
-		// 		))}
-		// 	</View>
-		// </View>
-    <>
-      <Goalpage/>
-    </>
+		<>
+			<Goalpage />
+		</>
 	);
 }
-
-// const styles = StyleSheet.create({
-// 	appContainer: {
-// 		flex: 1,
-// 		paddingTop: 50,
-// 		paddingHorizontal: 16,
-// 	},
-// 	inputContainer: {
-// 		flex: 1,
-// 		flexDirection: "row",
-// 		justifyContent: "space-between",
-// 		alignItems: "center",
-// 		marginBottom: 24,
-// 		borderBottomWidth: 1,
-// 		borderBottomColor: "#cccccc",
-// 	},
-// 	textInput: {
-// 		borderWidth: 1,
-// 		borderColor: "#cccccc",
-// 		width: "70%",
-// 		marginRight: 8,
-// 		padding: 8,
-// 	},
-// 	goalsContainer: {
-// 		flex: 5,
-// 	},
-// 	goalItem: {
-// 		margin: 8,
-// 		padding: 8,
-// 		borderRadius: 6,
-// 		backgroundColor: "#5e0acc",
-// 	},
-// 	goalText: {
-// 		color: "white",
-// 	},
-// });
